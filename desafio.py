9970d63549a4b702b5d630f2f907bed72b4b36ae
Out of the matrix <141779139+suelencosta1111@users.noreply.github.com>
2024-06-12 12:19:28 -0300

    Update desafio.py

--- desafio.py
+++ desafio.py
@@ -1,20 +1,20 @@
-# # Como usar botões e atalhos do teclado
-# import pyautogui
-# from time import sleep
-# # Navegar e clicar no campo email
-# pyautogui.click(2307,97, duration=2)
-# sleep(1)
-# # Digitar o email
-# pyautogui.typewrite("admin@hotmail.com")
-# sleep(1)
-# # Apertar enter
-# pyautogui.press("enter")
-# sleep(1)
-# # Digitar minha senha
-# pyautogui.typewrite("123456789")
-# sleep(1)
-# # Apertar enter
-# pyautogui.press("enter")
-# sleep(1)
-# # Apertar enter
-# pyautogui.press("enter")
+# Como usar botões e atalhos do teclado
+import pyautogui
+from time import sleep
+# Navegar e clicar no campo email
+pyautogui.click(2307,97, duration=2)
+sleep(1)
+# Digitar o email
+pyautogui.typewrite("admin@hotmail.com")
+sleep(1)
+# Apertar enter
+pyautogui.press("enter")
+sleep(1)
+# Digitar minha senha
+pyautogui.typewrite("123456789")
+sleep(1)
+# Apertar enter
+pyautogui.press("enter")
+sleep(1)
+# Apertar enter
+pyautogui.press("enter")
